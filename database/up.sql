b93f2620ced313916bb4e0f13db5f842dfbf1581
Daniel Luis <daluisgarcia@gmail.com>
2022-11-28 16:54:23 -0400

    Student enrollment in tests added to models and repositories

--- database/up.sql
+++ database/up.sql
@@ -1,6 +1,4 @@
 DROP TABLE IF EXISTS students;
-DROP TABLE IF EXISTS tests;
-DROP TABLE IF EXISTS questions;
 
 CREATE TABLE students (
     id VARCHAR(32) PRIMARY KEY,
@@ -8,15 +6,28 @@ CREATE TABLE students (
     age INTEGER NOT NULL
 )
 
+DROP TABLE IF EXISTS tests;
+
 CREATE TABLE tests (
     id VARCHAR(32) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
 )
+
+DROP TABLE IF EXISTS questions;
 
 CREATE TABLE questions (
     id VARCHAR(32) PRIMARY KEY,
     test_id VARCHAR(32) NOT NULL,
     question VARCHAR(255) NOT NULL,
     answer VARCHAR(255) NOT NULL,
+    FOREIGN KEY (test_id) REFERENCES tests(id)
+)
+
+DROP TABLE IF EXISTS enrollments;
+
+CREATE TABLE enrollments (
+    student_id VARCHAR(32) NOT NULL,
+    test_id VARCHAR(32) NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES students(id),
     FOREIGN KEY (test_id) REFERENCES tests(id)
 )
