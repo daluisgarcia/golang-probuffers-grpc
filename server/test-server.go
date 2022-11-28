d108349ad5bdbb4c1d63e288efd56cbff118d995
Daniel Luis <daluisgarcia@gmail.com>
2022-11-28 16:07:59 -0400

    Method in streaming by client side implemented for questions

--- server/test-server.go
+++ server/test-server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"io"
 
 	"github.com/daluisgarcia/golang-probuffers-grpc/models"
 	"github.com/daluisgarcia/golang-probuffers-grpc/repository"
@@ -47,4 +48,35 @@ func (s *TestServer) SetTest(ctx context.Context, req *testpb.Test) (*testpb.Set
 		Id:   test.Id,
 		Name: test.Name,
 	}, nil
+}
+
+// The type of the stream receives the requests during the connection is up and this function processes the requests
+func (s *TestServer) SetQuestions(stream testpb.TestService_SetQuestionsServer) error {
+	for {
+		msg, err := stream.Recv()
+
+		if err == io.EOF {
+			// The client has finished sending data
+			return stream.SendAndClose(&testpb.SetQuestionResponse{
+				Ok: true,
+			})
+		}
+
+		if err != nil {
+			return err
+		}
+
+		var question = &models.Question{
+			Id:       msg.Id,
+			Question: msg.Question,
+			TestId:   msg.TestId,
+			Answer:   msg.Answer,
+		}
+
+		err = s.repo.SetQuestion(stream.Context(), question)
+
+		if err != nil {
+			return err
+		}
+	}
 }
